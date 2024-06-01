ff6da16b97b17c7973c83c43ed522a9980278be8
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-06-01 15:52:26 +0530

    ADD

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
 //your JS code here. If required.
-let inputElements = document.getElementsByClassName("code");
-console.log(inputElements.length)
-for(let i=0; i< inputElements.length; i++){
-	inputElements[i].addEventListener("keyup", (event)=>{
-		let currentElement = event.target ;
-		let code = event.key.charCodeAt(0);
-		console.log(event.key)
-		if(event.key === "Backspace"){
-			let prevElement = currentElement.previousElementSibling ;
-			if(prevElement){
-				prevElement.focus();
-			}
-			return ;
-		}
+const codes = document.querySelectorAll('.code');
 
+codes.forEach((code, idx) => {
+    code.addEventListener('input', () => {
+        if (code.value.length > 0 && idx < codes.length - 1) {
+            codes[idx + 1].focus();
+        }
+    });
 
-
-		if(code>=48 && code<=57){
-			let nextElement = currentElement.nextElementSibling ;
-			if(nextElement){
-				nextElement.focus() ;
-			}
-
-		}else{
-			event.target.value="" ;
-		}
-	})
-
-}
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            if (code.value.length === 0 && idx > 0) {
+                codes[idx - 1].focus();
+            }
+        }
+    });
+});
